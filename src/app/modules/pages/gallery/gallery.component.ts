16dd7f3da071dff6d751eddc21367e9ad1c2e626
JhonAguiar <jhon.aguiar93@gmail.com>
2024-12-30 14:11:26 -0500

    Ajustes

--- src/app/modules/pages/gallery/gallery.component.ts
+++ src/app/modules/pages/gallery/gallery.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './gallery.component.scss'
 })
 export class GalleryComponent {
+  currentPage = 1;
+  blogsPerPage = 10;
+  totalPages: number[] = [];
+  
+  blogs = [];
 
+  constructor() {
+    this.calculateTotalPages();
+  }
+
+  // Calcula el número total de páginas
+  calculateTotalPages() {
+    const totalBlogs = this.blogs.length;
+    const pages = Math.ceil(totalBlogs / this.blogsPerPage);
+    this.totalPages = Array.from({ length: pages }, (_, i) => i + 1);
+  }
+
+  // Cambia de página
+  changePage(page: number) {
+    if (page < 1 || page > this.totalPages.length) return;
+    this.currentPage = page;
+  }
+
+  // Devuelve los blogs que se deben mostrar para la página actual
+  get blogsToShow() {
+    const startIndex = (this.currentPage - 1) * this.blogsPerPage;
+    return this.blogs.slice(startIndex, startIndex + this.blogsPerPage);
+  }
 }
