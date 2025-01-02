c168fc5eafc990d516e58eebdbb8bd1837d9b8f7
JhonAguiar <jhon.aguiar93@gmail.com>
2025-01-02 13:13:05 -0500

    Ajustes

--- src/app/core/guards/AuthGuard.guard.ts
+++ src/app/core/guards/AuthGuard.guard.ts
@@ -13,26 +13,30 @@ export class AuthGuard implements CanActivate {
 
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-
-        let isAuthenticated = false;
-        console.log(isAuthenticated)
-        // Verificar si estamos en el navegador antes de acceder a localStorage
-        if (typeof window !== 'undefined' && window.localStorage) {
-          // Solo accedemos a localStorage si está disponible en el navegador
-          if (localStorage.getItem('login') || localStorage.getItem('login') !== undefined) {
-            isAuthenticated = true;
-          }
+      let isAuthenticated = false;
+      console.log(isAuthenticated);
+    
+      // Verificar si estamos en el navegador antes de acceder a localStorage
+      if (typeof window !== 'undefined' && window.localStorage) {
+        // Solo accedemos a localStorage si está disponible en el navegador
+        const loginItem = localStorage.getItem('login');
+        
+        // Verificar si el valor 'login' está presente en localStorage
+        if (loginItem !== null && loginItem !== undefined) {
+          isAuthenticated = true;
         }
-
-        console.log(typeof window !== 'undefined' && window.localStorage)
-
-        if (!isAuthenticated) {
-            this.router.navigate(['/login']);
-
-            return false;
-        }
-
-        return true; 
+      }
+    
+      console.log('window.localStorage available:', typeof window !== 'undefined' && window.localStorage);
+    
+      if (!isAuthenticated) {
+        // Si no está autenticado, redirigimos al login
+        this.router.navigate(['/login']);
+        return false;
+      }
+    
+      // Si está autenticado, permitimos el acceso
+      return true;
     }
 
 }
